c4913fe21ec55a8260de3f667a871816f995668c
Orhun Melih Sabah <111877443+orhunsabah@users.noreply.github.com>
2023-08-21 11:01:25 +0300

    Add files via upload

---
+++ BasicCRUDAppEF/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicCRUDAppEF
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+       
+        public string ?FirstName { get; set; }
+
+        
+        public string ?LastName { get; set; }
+    }
+}
+
