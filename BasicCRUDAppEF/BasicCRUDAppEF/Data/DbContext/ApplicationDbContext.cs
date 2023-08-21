340b889a603ecec735d1c735b43e610c21868261
Orhun Melih Sabah <111877443+orhunsabah@users.noreply.github.com>
2023-08-21 10:41:56 +0300

    Add files via upload

---
+++ BasicCRUDAppEF/BasicCRUDAppEF/Data/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicCRUDAppEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=postgres;Username=postgres;Password=postgres;");
+        }
+
+    }
+
+}
+
